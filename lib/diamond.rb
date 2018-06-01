a13da76bbfcf8fafd95b55ef364fc895396ac448
Nicolas Zermati <nicoolas25@gmail.com>
2018-06-01 22:38:50 +0200

    Green tests, shameless code using symetries

--- lib/diamond.rb
+++ lib/diamond.rb
@@ -1,2 +1,17 @@
 module Diamond
+  SPACE = "-".freeze
+  LETTERS = ("A".."Z").to_a.freeze
+
+  def self.print_diamond(letter)
+    height = LETTERS.index(letter)
+
+    quarters = (0..height).map do |current_height|
+      line = Array.new(height + 1, SPACE)
+      line[current_height] = LETTERS[current_height]
+      line
+    end
+
+    quarters.each { |row| puts row.reverse.join + row[1..-1].join }
+    quarters.reverse[1..-1].each { |row| puts row.reverse.join + row[1..-1].join }
+  end
 end
--- spec/diamond_spec.rb
+++ spec/diamond_spec.rb
@@ -1,5 +1,42 @@
 RSpec.describe Diamond do
-  it "works" do
-    expect(true).to be true
+  describe ".print_diamond" do
+    subject(:print_diamond) { ->(letter) { described_class.print_diamond(letter) } }
+
+    it "works for A" do
+      expect { print_diamond["A"] }.to output(<<~EXPECTED).to_stdout
+        A
+      EXPECTED
+    end
+
+    it "works for B" do
+      expect { print_diamond["B"] }.to output(<<~EXPECTED).to_stdout
+        -A-
+        B-B
+        -A-
+      EXPECTED
+    end
+
+    it "works for C" do
+      expect { print_diamond["C"] }.to output(<<~EXPECTED).to_stdout
+        --A--
+        -B-B-
+        C---C
+        -B-B-
+        --A--
+      EXPECTED
+    end
+
+    it "works for D" do
+      expect { print_diamond["D"] }.to output(<<~EXPECTED).to_stdout
+        ---A---
+        --B-B--
+        -C---C-
+        D-----D
+        -C---C-
+        --B-B--
+        ---A---
+      EXPECTED
+    end
+
   end
 end
